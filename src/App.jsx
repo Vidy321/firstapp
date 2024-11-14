9c751eda9423c1d09412760742c63e20ded410f2
rajumb0232 <rajugowda0232@gmail.com>
2024-11-14 11:19:42 +0530

    app.jsx is edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import React, { useState } from 'react'
-import ListItem from './ListItem'
+import React from 'react'
 
 function App() {
-  let s=[{id:1,name:"miller"},{id:2,name:"jhon"},
-    {id:3,name:"allen"},{id:4,name:"bablu"}
-  ]
-
-  let [students,setstudents]=useState(s);
-
-  let updateStudents=()=>
-  {
-    setstudents([{id:5,name:"smith"},...students])
-  }
   return (
-    <>
-      <ul>
-      {students.map((student,index)=>
-      {
-        return <ListItem key={student.id} 
-        student={student}/>
-      })}
-    </ul>
-    <button onClick={updateStudents}>
-      Update Students</button>
-    </>
+    <div>
+      <h1>React App</h1>
+    </div>
   )
 }
 
